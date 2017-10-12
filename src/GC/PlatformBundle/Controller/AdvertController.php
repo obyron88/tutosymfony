9aa398aee3c6b0fd7faabb7339c6eea92cfeb05a
obyron88 <coda2017@yahoo.com>
2017-10-12 16:39:57 +0200

    eee

--- src/GC/PlatformBundle/Controller/AdvertController.php
+++ src/GC/PlatformBundle/Controller/AdvertController.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 // pour l'url complete
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -23,81 +24,133 @@ class AdvertController extends Controller
     // on doit donc définir la méthode viewAction.
     // On donne à cette méthode l'argument $id, pour
     // correspondre au paramètre {id} de la route
-    public function viewAction($id, Request $request)
+    public function viewAction($id)
     {
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
 
-        // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
-        // Ici, on récupèrera depuis la base de données
-        // l'annonce correspondant à l'id $id.
-        // Puis on passera l'annonce à la vue pour
-        // qu'elle puisse l'afficher
-//        return new Response("Affichage de l'annonce d'id : ".$id);
-//        $tag = $request->query->get('tag');
-//
-//        return new Response(
-//            "Affichage de l'annonce d'id : ".$id.", avec le tag : ".$tag
-
-//        );// On crée la réponse sans lui donner de contenu pour le moment
-//        $response = new Response();
-//        // On définit le contenu
-//        $response->setContent("Ceci est une page d'erreur 404");
-//        // On définit le code HTTP à « Not Found » (erreur 404)
-//        $response->setStatusCode(Response::HTTP_NOT_FOUND);
-//        // On retourne la réponse
-//        return $response;
-        // On récupère notre paramètre tag
-//        $tag = $request->query->get('tag');
-//        // On utilise le raccourci : il crée un objet Response
-//        // Et lui donne comme contenu le contenu du template
-//        return $this->get('templating')->renderResponse(
-//            'GCPlatformBundle:Advert:view.html.twig',
-//            array('id'  => $id, 'tag' => $tag)
-//        );
-//        $url = $this->get('router')->generate('gc_platform_home');
-//
-//        return new RedirectResponse($url);
-        //pareil que les 2 ligne au dessus mais moins long
-//        return $this->redirectToRoute('gc_platform_home');
-        // Créons nous-mêmes la réponse en JSON, grâce à la fonction json_encode()
-//        $response = new Response(json_encode(array('id' => $id)));
-//        // Ici, nous définissons le Content-type pour dire au navigateur
-//        // que l'on renvoie du JSON et non du HTML
-//        $response->headers->set('Content-Type', 'application/json');
-//        return $response;
-//        return new JsonResponse(array('id' => $id));
-        // Récupération de la session
-
-        $session = $request->getSession();
-
-        // On récupère le contenu de la variable user_id
-        $userId = $session->get('user_id');
-
-        // On définit une nouvelle valeur pour cette variable user_id
-        $session->set('user_id', 91);
-
-        // On n'oublie pas de renvoyer une réponse
-        return new Response("<body>Je suis une page de test, je n'ai rien à dire.</body>");
+        return $this->render('GCPlatformBundle:Advert:view.html.twig', array(
+            'advert' => $advert
+        ));
     }
-    public function indexAction()
+    // Ajoutez cette méthode :
+
+    public function addAction(Request $request)
+    {
+        // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+
+        // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+        if ($request->isMethod('POST')) {
+            // Ici, on s'occupera de la création et de la gestion du formulaire
+
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+            // Puis on redirige vers la page de visualisation de cettte annonce
+            return $this->redirectToRoute('gc_platform_view', array('id' => 5));
+        }
+
+        // Si on n'est pas en POST, alors on affiche le formulaire
+        return $this->render('GCPlatformBundle:Advert:add.html.twig');
+    }
+    public function editAction($id, Request $request)
+    {
+        // Ici, on récupérera l'annonce correspondante à $id
+
+        // Même mécanisme que pour l'ajout
+        if ($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+
+            return $this->redirectToRoute('gc_platform_view', array('id' => 5));
+        }
+
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+
+        return $this->render('GCPlatformBundle:Advert:edit.html.twig', array(
+            'advert' => $advert
+        ));
+    }
+
+    public function deleteAction($id)
+    {
+        // Ici, on récupérera l'annonce correspondant à $id
+
+        // Ici, on gérera la suppression de l'annonce en question
+
+        return $this->render('GCPlatformBundle:Advert:delete.html.twig');
+    }
+    public function indexAction($page)
     {
         // On veut avoir l'URL de l'annonce d'id 5.
 //        $url = $this->get('router')->generate(
 //            'gc_platform_view', // 1er argument : le nom de la route
 //            array('id' => 5)    // 2e argument : les valeurs des paramètres
 //        );
-//        pour generer l'url entier
-                $url = $this->get('router')->generate('gc_platform_home', array(), UrlGeneratorInterface::ABSOLUTE_URL);
-        // $url vaut « /platform/advert/5 »
-        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
+////        pour generer l'url entier
+//                $url = $this->get('router')->generate('gc_platform_home', array(), UrlGeneratorInterface::ABSOLUTE_URL);
+//        // $url vaut « /platform/advert/5 »
+//        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
+
+        // On ne sait pas combien de pages il y a
+        // Mais on sait qu'une page doit être supérieure ou égale à 1
+        if ($page < 1) {
+            // On déclenche une exception NotFoundHttpException, cela va afficher
+            // une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
+            throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
     }
-//    public function indexAction()
-//    {
-//
-//        $content = $this
-//            ->get('templating')
-//            ->render('GCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzou'));
-//
-//
-//        return new Response($content);
-//    }
+        // Ici, on récupérera la liste des annonces, puis on la passera au template
+        // Mais pour l'instant, on ne fait qu'appeler le template
+        return $this->render('GCPlatformBundle:Advert:index.html.twig', array(
+            'listAdverts' => array(
+                array(
+                    'title'   => 'Recherche développpeur Symfony',
+                    'id'      => 1,
+                    'author'  => 'Alexandre',
+                    'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+                    'date'    => new \Datetime()),
+                array(
+                    'title'   => 'Mission de webmaster',
+                    'id'      => 2,
+                    'author'  => 'Hugo',
+                    'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                    'date'    => new \Datetime()),
+                array(
+                    'title'   => 'Offre de stage webdesigner',
+                    'id'      => 3,
+                    'author'  => 'Mathieu',
+                    'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                    'date'    => new \Datetime())
+            )));
+        // Et modifiez le 2nd argument pour injecter notre liste
+        return $this->render('GCPlatformBundle:Advert:index.html.twig', array(
+            'listAdverts' => $listAdverts
+        ));
+
+    }
+    public function menuAction($limit)
+    {
+        // On fixe en dur une liste ici, bien entendu par la suite
+        // on la récupérera depuis la BDD !
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+            array('id' => 5, 'title' => 'Mission de webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
+
+        return $this->render('GCPlatformBundle:Advert:menu.html.twig', array(
+            // Tout l'intérêt est ici : le contrôleur passe
+            // les variables nécessaires au template !
+            'listAdverts' => $listAdverts
+        ));
+    }
 }
